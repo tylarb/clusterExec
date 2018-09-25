2cf32aca80b4be3b50a0441f39759bec5f1c25af
Tyler Ramer <tyaramer@gmail.com>
2018-09-25 12:42:36 -0400

    hacky fix for broken ssh-keyscan on travis

--- ci/keyscan.sh
+++ ci/keyscan.sh
@@ -4,7 +4,7 @@
 ssh-keyscan  172.22.0.{10..13}  > $TRAVIS_BUILD_DIR/known_hosts
 
 # cluster25
-ssh-keyscan -p 25 172.25.0.{10..13}  >> $TRAVIS_BUILD_DIR/known_hosts
+ssh-keyscan -p 25 172.25.0.{10..13}  | awk '{print "["$1"]:25 " $2 " " $3}' >> $TRAVIS_BUILD_DIR/known_hosts
 
 
 cat $TRAVIS_BUILD_DIR/known_hosts
