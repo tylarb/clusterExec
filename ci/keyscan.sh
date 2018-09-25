a9206d64609f7bff64381e345d3b7a0eed0e6494
Tyler Ramer <tyaramer@gmail.com>
2018-09-25 11:57:33 -0400

    still having issues on travis

--- ci/keyscan.sh
+++ ci/keyscan.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # cluster22 
-ssh-keyscan 172.22.0.{10..13}  > $TRAVIS_BUILD_DIR/known_hosts
+ssh-keyscan -v 172.22.0.{10..13}  > $TRAVIS_BUILD_DIR/known_hosts
 
 # cluster25
-ssh-keyscan -p 25 172.25.0.{10..13}  >> $TRAVIS_BUILD_DIR/known_hosts
+ssh-keyscan -vp 25 172.25.0.{10..13}  >> $TRAVIS_BUILD_DIR/known_hosts
 
 
 cat $TRAVIS_BUILD_DIR/known_hosts
